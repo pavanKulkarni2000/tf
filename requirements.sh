429c4212d34d3e411480e416954bbd170786e1fe
Pavan K R <43609751+pavanKulkarni2000@users.noreply.github.com>
2020-07-10 01:17:03 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,8 @@
 
 # Get packages required for OpenCV
 
+# run as ./requirements
+
 sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
